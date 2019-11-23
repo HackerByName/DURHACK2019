6cc91c2d282d706c9c01a97d4020d36acdf22b47
fboyle2001 <7895247+fboyle2001@users.noreply.github.com>
2019-11-23 20:04:51 +0000

    Styling

--- flask_site/helper.py
+++ flask_site/helper.py
@@ -3,8 +3,21 @@ from flask_site import *
 class Verifications:
     @staticmethod
     def is_logged_in():
-        return "email" in session
+        return "user" in session
 
     def logout():
         if Verifications.is_logged_in():
-            session.pop("email", None)
+            session.pop("user", None)
+
+class User:
+    @staticmethod
+    def from_record(record):
+        return User(record["_id"], record["first_name"], record["last_name"],
+         record["email"], record["accounts"])
+
+    def __init__(self, id, first_name, last_name, email, accounts):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.accounts = accounts
--- flask_site/routes.py
+++ flask_site/routes.py
@@ -2,17 +2,17 @@ from flask_site import *
 from flask import render_template, flash, redirect
 from flask_site.forms import LoginForm, RegisterForm
 from .mongo import MongoDatabase
-from .helper import Verifications
+from .helper import Verifications, User
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("base.html")
+    return render_template("base.html", title="Homepage", user=(session["user"] if "user" in session else None))
 
 @app.route("/register", methods=["GET"])
 def register():
     register_form = RegisterForm()
-    return render_template("register.html", form=register_form)
+    return render_template("register.html", form=register_form, title="Register", user=(session["user"] if "user" in session else None))
 
 @app.route("/register", methods=["POST"])
 def process_register():
@@ -24,11 +24,11 @@ def process_register():
         first_name = form.first_name.data
         last_name = form.last_name.data
         hashed = bcrypt.generate_password_hash(password).decode("utf-8")
-        MongoDatabase.insertNewUser(student_records, first_name, last_name, email, hashed)
+        MongoDatabase.insert_new_user(student_records, first_name, last_name, email, hashed)
         #flash("{}, {}, {}, {}".format(email, password, first_name, last_name))
         return redirect("/login")
 
-    return render_template("register.html", title="Register", form=form)
+    return render_template("register.html", title="Register", form=form, user=(session["user"] if "user" in session else None))
 
 @app.route("/login", methods=["GET"])
 def login():
@@ -37,7 +37,7 @@ def login():
         return redirect("/index")
 
     login_form = LoginForm()
-    return render_template("login.html", form=login_form)
+    return render_template("login.html", form=login_form, title="Login", user=(session["user"] if "user" in session else None))
 
 @app.route("/login", methods=["POST"])
 def process_login():
@@ -57,12 +57,12 @@ def process_login():
             flash("Wrong password")
             return redirect("/index")
 
-        session["email"] = email
+        session["user"] = User.from_record(user_record)
 
         flash("Logged in, {}".format(email))
         return redirect("/index")
 
-    return render_template("login.html", title="Login", form=form)
+    return render_template("login.html", title="Login", form=form, user=(session["user"] if "user" in session else None))
 
 @app.route("/logout")
 def logout():
